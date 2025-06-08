1d5a098c7e41339c77c0ffad1d5e9e43ab597eae
Thopireddy HarikishanReddy <103351345+HarikishanReddy2004@users.noreply.github.com>
2025-06-08 21:50:37 +0530

    place necessary tokens in config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+JIRA_BASE_URL = "https://********.atlassian.net"
+JIRA_API_TOKEN = "***-***-***-***"
+gemini_api="******************"
+JIRA_EMAIL = "***************@gmail.com"
+BOARD_ID = 1  # your Jira board ID
