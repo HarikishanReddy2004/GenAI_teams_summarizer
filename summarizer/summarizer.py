0d09fde92d9b9ad152d98f53b924aaebe154bec6
Thopireddy HarikishanReddy <103351345+HarikishanReddy2004@users.noreply.github.com>
2025-06-19 19:04:57 +0530

    Update summarizer.py

--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -1,15 +1,3 @@
-# # summarizer/summarizer.py
-#
-# from transformers import pipeline
-# # Load Hugging Face summarizer once
-# summarization_pipeline = pipeline("summarization", model="facebook/bart-large-cnn")
-#
-# def generate_summary(text):
-#     if len(text) < 100:
-#         text += ". " * (100 - len(text))  # Padding trick for very short text
-#
-#     summary = summarization_pipeline(text, max_length=60, min_length=15, do_sample=False)
-#     return summary[0]['summary_text']
 import os
 import requests
 from config import gemini_api
@@ -18,7 +6,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", gemini_api)
 
 GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
 
-def generate_summary(text, instruction="Summarize the following text clearly and concisely into two points  by removing unwanted discussion or random talk"):
+def generate_summary(text, instruction="Summarize the following text clearly and concisely into two points  by removing unwanted discussion or random talk and remove headings like **Summary** ,**Updates** etc.."):
     if not text.strip():
         return "Input text is empty."
 
